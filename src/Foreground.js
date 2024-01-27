8521e077e98fce2e11e743cd3ac45ca0c9813761
David Rubin <david@lagunahealth.com>
2024-01-27 14:10:20 +0200

    aaa

--- src/Foreground.js
+++ src/Foreground.js
@@ -1,12 +1,23 @@
 import Phaser from "phaser";
 import { DancingLight } from "./DancingLight";
 import { gameMap } from './maps'
+import { Heartbeat } from "./HeartbeatService";
 
 
 const HEIGHT = gameMap.length * 32;
 const WIDTH = gameMap[0].length;
 const placeFromBottom = (x, y) => {
   return [x * 32, (gameMap.length - y) * 32]
+}
+
+const MAX = {
+  scale: 1.2,
+  alpha: 1,
+}
+
+const MIN = {
+  scale: 1,
+  alpha: 1,
 }
 
 export class Foreground extends Phaser.GameObjects.Container {
@@ -23,5 +34,50 @@ export class Foreground extends Phaser.GameObjects.Container {
 
     this.add(new DancingLight(scene, ...placeFromBottom(0, 25)));
     this.add(new DancingLight(scene, ...placeFromBottom(WIDTH, 25)));
+
+
+    this.add(scene.add.sprite(scene.scale.gameSize.width / 2, 32, "character", "chair"));
+    const gold1 = scene.add.sprite(scene.scale.gameSize.width / 2 - 64, 96, "character", "plank2");
+    this.add(gold1);
+    const gold2 = scene.add.sprite(scene.scale.gameSize.width / 2 + 64, 96, "character", "plank2");
+    this.add(gold2);
+
+    this.add(scene.add.sprite(scene.scale.gameSize.width / 2, HEIGHT - 7 * 32, "character", "lamp"));
+
+    const plank1 = scene.add.sprite(scene.scale.gameSize.width / 2 - 96, HEIGHT - 13 * 32, "character", "plank1");
+    const plank2 = scene.add.sprite(scene.scale.gameSize.width / 2 + 96, HEIGHT - 13 * 32, "character", "plank1");
+
+    const p1 = scene.add.sprite(...placeFromBottom(2.5, 22), "character", "plank1");
+    this.add(p1);
+    const p2 = scene.add.sprite(...placeFromBottom(5.5, 19), "character", "plank1");
+    this.add(p2);
+
+    this.add(plank1);
+    this.add(plank2);
+    Heartbeat.eventEmitter.addEventListener("beat", (e) => {
+      const even = !!((e.detail.beatCount) % 2);
+
+      scene.tweens.add({
+        targets: [even ? plank1 : plank2],
+        ...MAX,
+        duration: 200,
+        yoyo: true,
+      });
+      scene.tweens.add({
+        targets: [even ? gold2 : gold1],
+        ...MAX,
+        duration: 200,
+        yoyo: true,
+      });
+      scene.tweens.add({
+        targets: [even ? p1 : p2],
+        ...MAX,
+        duration: 200,
+        yoyo: true,
+      });
+
+
+    });
+
   }
 }
--- src/StageBackground.js
+++ src/StageBackground.js
@@ -86,7 +86,8 @@ export class StageBackground extends Phaser.GameObjects.Container {
   }
 
   getStartingPoint() {
-    return new Phaser.Math.Vector2(4, this.rows - 35);
+    // return new Phaser.Math.Vector2(4, this.rows - 35);
+    return new Phaser.Math.Vector2(3, this.rows - 2);
   }
 
   isWin(x, y) {
